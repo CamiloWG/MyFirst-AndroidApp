ca68f0372c392579c5977750ef8e04a311cc492a
CamiloWG <millos2423@gmail.com>
2022-05-27 19:30:00 -0500

    First Commit

---
+++ XamarinApp/XamarinApp/Vistas/ConvertPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConvertPage : ContentPage
+    {
+        public double metros;
+        public double pulgadas;
+        public ConvertPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular()
+        {
+            metros = Convert.ToDouble(txtInput.Text);
+            pulgadas = metros * 39.3701;
+        }
+
+        private async void btnVolver_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtInput.Text))
+            {
+                this.Calcular();
+                txtResult.Text = Convert.ToString(this.pulgadas) + " pulgadas";
+            }
+            else DisplayAlert("Error", "Por favor ingrese un número", "Aceptar");
+        }
+    }
+}
---
+++ XamarinApp/XamarinApp/Vistas/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinApp.Vistas;
+
+namespace XamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConvertPage());
+        }
+    }
+}
